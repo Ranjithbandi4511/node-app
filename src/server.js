f314dad776f3614cc32fbb71c49886cd78237963
Ranjith <ranjithkumarbandi004@gmail.com>
2026-05-31 22:56:30 +0530

    3rd commit

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-  res.json({ status: "ok ok" });
+  res.json({ status: "ok ok ok" });
 });
 
 app.use("/api/users", usersRouter);
